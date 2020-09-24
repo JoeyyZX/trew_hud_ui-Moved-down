83c6579698b077278939ecd3f742f42fbf9c13ba
JoeyyZX <65097884+JoeyyZX@users.noreply.github.com>
2020-09-24 16:43:36 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,68 @@
+Config = {}
+
+Config.Locale = 'en'
+
+Config.serverLogo = ''
+
+Config.font = {
+	name 	= 'Montserrat',
+	url 	= 'https://fonts.googleapis.com/css?family=Montserrat:300,400,700,900&display=swap'
+}
+
+Config.date = {
+	format	 	= 'default',
+	AmPm		= false
+}
+
+Config.voice = {
+
+	levels = {
+		default = 5.0,
+		shout = 12.0,
+		whisper = 1.0,
+		current = 0
+	},
+	
+	keys = {
+		distance 	= 'Z',
+	}
+}
+
+
+Config.vehicle = {
+	speedUnit = 'MPH',
+	maxSpeed = 500,
+
+	keys = {
+		seatbelt 	= 'B',
+		cruiser		= 'CAPS',
+		signalLeft	= 'LEFT',
+		signalRight	= 'RIGHT',
+		signalBoth	= 'DOWN',
+	}
+}
+
+Config.ui = {
+	showServerLogo		= false,
+
+	showJob		 		= true,
+
+	showWalletMoney 	= true,
+	showBankMoney 		= true,
+	showBlackMoney 		= true,
+	showSocietyMoney	= true,
+
+	showDate 			= true,
+	showLocation 		= true,
+	showVoice	 		= true,
+
+	showHealth			= true,
+	showArmor	 		= true,
+	showStamina	 		= true,
+	showHunger 			= true,
+	showThirst	 		= true,
+
+	showMinimap			= true,
+
+	showWeapons			= true,	
+}
